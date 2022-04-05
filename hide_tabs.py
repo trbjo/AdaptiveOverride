c0faef0b7f4a691bf1e95c5f835c66cec60529d1
Troels Bjørnskov <90050908+trobjo-telia@users.noreply.github.com>
2022-04-05 11:00:56 +0200

    Add LineHighlight

--- hide_tabs.py
+++ hide_tabs.py
@@ -14,3 +14,10 @@ class ViewTabs(sublime_plugin.EventListener):
     def on_new_window_async(self, window):
         if len(sublime.active_window().sheets()) < 2:
             sublime.active_window().set_tabs_visible(False)
+
+class LineHighlight(sublime_plugin.ViewEventListener):
+    def on_activated_async(self):
+        self.view.settings().set("highlight_line", True)
+
+    def on_deactivated_async(self):
+        self.view.settings().set("highlight_line", False)
