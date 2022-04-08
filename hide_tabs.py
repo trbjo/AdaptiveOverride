053e5e0c7089bd7a799445951cc0e55442f35515
Troels Bjørnskov <troels@bjoernskov.org>
2022-04-08 21:58:05 +0200

    Use status bar, not tabs

--- hide_tabs.py
+++ hide_tabs.py
@@ -1,23 +1,23 @@
-import sublime
-import sublime_plugin
+# import sublime
+# import sublime_plugin
 
-class ViewTabs(sublime_plugin.EventListener):
+# class ViewTabs(sublime_plugin.EventListener):
 
-    def on_activated_async(self, view):
-        if len(sublime.active_window().sheets()) > 1:
-            sublime.active_window().set_tabs_visible(True)
+#     def on_activated_async(self, view):
+#         if len(sublime.active_window().sheets()) > 1:
+#             sublime.active_window().set_tabs_visible(True)
 
-    def on_close(self, view):
-        if len(sublime.active_window().sheets()) < 2:
-            sublime.active_window().set_tabs_visible(False)
+#     def on_close(self, view):
+#         if len(sublime.active_window().sheets()) < 2:
+#             sublime.active_window().set_tabs_visible(False)
 
-    def on_new_window_async(self, window):
-        if len(sublime.active_window().sheets()) < 2:
-            sublime.active_window().set_tabs_visible(False)
+#     def on_new_window_async(self, window):
+#         if len(sublime.active_window().sheets()) < 2:
+#             sublime.active_window().set_tabs_visible(False)
 
-class LineHighlight(sublime_plugin.ViewEventListener):
-    def on_activated_async(self):
-        self.view.settings().set("highlight_line", True)
+# class LineHighlight(sublime_plugin.ViewEventListener):
+#     def on_activated_async(self):
+#         self.view.settings().set("highlight_line", True)
 
-    def on_deactivated_async(self):
-        self.view.settings().set("highlight_line", False)
+#     def on_deactivated_async(self):
+#         self.view.settings().set("highlight_line", False)
