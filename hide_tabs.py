5673a92452c918b542def023de13e3b3ede375a7
Troels Bjørnskov <troels@bjoernskov.org>
2022-05-19 12:12:46 +0200

    Activate hide_tabs.py

--- hide_tabs.py
+++ hide_tabs.py
@@ -1,19 +1,37 @@
-# import sublime
-# import sublime_plugin
+import sublime
+import sublime_plugin
 
-# class ViewTabs(sublime_plugin.EventListener):
+class ViewTabs(sublime_plugin.EventListener):
 
-#     def on_activated_async(self, view):
-#         if len(sublime.active_window().sheets()) > 1:
-#             sublime.active_window().set_tabs_visible(True)
+    def on_new_window(self, window):
+        if len(window.sheets()) > 1:
+            window.set_tabs_visible(True)
+        else:
+            window.set_tabs_visible(False)
 
-#     def on_close(self, view):
-#         if len(sublime.active_window().sheets()) < 2:
-#             sublime.active_window().set_tabs_visible(False)
+    def on_load_project(self, window):
+        if len(window.sheets()) > 1:
+            window.set_tabs_visible(True)
+        else:
+            window.set_tabs_visible(False)
 
-#     def on_new_window_async(self, window):
-#         if len(sublime.active_window().sheets()) < 2:
-#             sublime.active_window().set_tabs_visible(False)
+    def on_new_project(self, window):
+        if len(window.sheets()) > 1:
+            window.set_tabs_visible(True)
+        else:
+            window.set_tabs_visible(False)
+
+    def on_activated_async(self, view):
+        if len(sublime.active_window().sheets()) > 1:
+            sublime.active_window().set_tabs_visible(True)
+
+    def on_close(self, view):
+        if len(sublime.active_window().sheets()) < 2:
+            sublime.active_window().set_tabs_visible(False)
+
+    def on_new_window_async(self, window):
+        if len(sublime.active_window().sheets()) < 2:
+            sublime.active_window().set_tabs_visible(False)
 
 # class LineHighlight(sublime_plugin.ViewEventListener):
 #     def on_activated_async(self):
